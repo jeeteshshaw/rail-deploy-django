552bab88bac8ddd008d8bf2b5bc9e3f4aff07be4
Jeetesh Kumar Shaw <jeeteshshaw@gmail.com>
2023-01-21 13:49:12 +0530

    1st commit

---
+++ polls/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpRequest, HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello world")
